cdaefd96240075d9a0a13745bb72e4b0886eefa5
brandester32 <brandondester@gmail.com>
2016-11-02 16:34:31 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,20 @@
 <HTML>
-<HEAD>
-</HEAD>
+	<HEAD>
+		<title>
+		Good Vibes Login
+		</title>
+	</HEAD>
 <BODY>
-
+	<img src="goodvibeslogo.jpg" />
+	<hr />
+	<h1>Login</h1>
 <form action="login_script.php" method="POST">
 
 <p>Username: <input type:"text" name="username" size="30"/></p>
 <p>Password: <input type:"text" name="password" size="30"/></p>
 
 <input type="submit" name="submit" value="Submit"/>
+</br>New User? <a href="createAccount.php">Create Account</a>
 
 
 
