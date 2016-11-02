bcbf41b163addc058c43dddbb848966a572bb78b
brandester32 <brandondester@gmail.com>
2016-11-02 16:33:05 -0400

    Add files via upload

---
+++ createAccount.php
@@ -0,0 +1,25 @@
+<HTML>
+	<HEAD>
+		<title>
+		Good Vibes Account Creation
+		</title>
+	</HEAD>
+<BODY>
+	<img src="goodvibeslogo.jpg" />
+	<hr />
+	<h1>Create New Account</h1>
+<form action="login_script.php" method="POST">
+
+<p>Enter Desired Username: <input type:"text" name="username" size="30"/></p>
+<p>Enter Desired Password: <input type:"text" name="password" size="30"/></p>
+<p>Re-Enter Desired Password: <input type:"text" name="password" size="30"/></p>
+
+<input type="submit" name="submit" value="Submit"/>
+
+
+
+
+
+
+</BODY>
+</HTML>
