fac3257b3d6a62f7470ab37b6deded3137f9e50c
MaxAukshunas <jaukshunas@umass.edu>
2016-11-10 17:20:05 -0500

    Add files via upload

--- login_script.php
+++ login_script.php
@@ -1,17 +1,28 @@
 <?PHP
-
-
-$username = $_POST['username'];
+	include('session.php');
+	print "welcome $logged_in_user";
+/*$username = $_POST['username'];
 $password = $_POST['password'];
+
 $info = $username . "," . $password;
 $file = "file.csv";
 
 file_put_contents($file, $info . PHP_EOL, FILE_APPEND);
-
-print "Thank you for entering your email and password.</br>The website will be working soon we promise.";
-
-
-
-
+*/
 
 ?>
+
+<HTML>
+	<HEAD>
+		<title>
+		Welcome!
+		</title>
+	</HEAD>
+<BODY>
+	
+	<h1> Welcome <?php print "$logged_in_user;" ?> </h1>
+	<h2><a href="logout.php">Log Out</a></h2>
+
+
+</BODY>
+</HTML>
---
+++ session.php
@@ -0,0 +1,17 @@
+<?php
+	$database = mysqli_connect("localhost","root","password","user_accounts"); // connect to database
+	
+	session_start(); // start the session
+	
+	$username = $_SESSION['logged_in_user']; // get the username of the user thats logged in
+	
+	$session = mysqli_query($database,"SELECT username FROM accounts WHERE username = '$username'"); // find the account of the user
+	
+	$row = mysqli_fetch_array($session,MYSQLI_ASSOC); // find the row of the account
+	
+	$logged_in_user = $row['username']; // get the username of the logged in user
+	
+	if(is_null($_SESSION['logged_in_user'])){ // if the user isn't logged in 
+		header("Location: loginpage.php"); // redirect to the log in page
+	}
+?>
