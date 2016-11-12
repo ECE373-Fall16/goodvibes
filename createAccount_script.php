de74e948f85cc30c532463d633deb7e0aa624271
MaxAukshunas <jaukshunas@umass.edu>
2016-11-12 17:40:39 -0500

    Update createAccount_script.php

--- createAccount_script.php
+++ createAccount_script.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	// connect to database
-	$db = mysqli_connect('localhost', 'root', 'password','authentication');
+	$db = mysqli_connect('localhost', 'root', 'password','user_accounts');
 	if(!$db){
 		echo "Not Connected to server";
 	}
@@ -9,11 +9,14 @@
 		$fname = $_POST['fname'];
 		$lname = $_POST['lname'];
 		$username = $_POST['username'];
+		$email = $_POST['email'];
 		$password = $_POST['password'];
 		$repassword = $_POST['repassword'];
 		if ($password == $repassword) {
 			// create user
-			$sql = "INSERT INTO users(fname, lname, username, password) VALUES('$fname','$lname','$username', '$repassword')";
+			$sql = "INSERT INTO accounts(id, fname, lname, username, email, password) VALUES(NULL, '$fname','$lname','$username', '$email', '$repassword')";
+			$sql2 = "INSERT INTO profiles(id, username, genre, location, instruments, age, experience) VALUES(NULL, '$username', '','','',0,0)";
+			$addprofile = mysqli_query($db, $sql2);
 			if(mysqli_query($db, $sql)){
 				echo "New record created successfully";
 			}else{
@@ -21,7 +24,7 @@
 			}
 			//$_SESSION['message'] = "You are now logged in";
 			//$_SESSION['username'] = $username;
-			header("location: login.php"); //redirect to home page
+			header("location: loginpage.php"); //redirect to home page
 		}else{
 			$_SESSION['message'] = "The two passwords do not match";
 		}
