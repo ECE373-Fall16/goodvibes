142e9749854d73d21e3f3b3dc5fc540ffec07f5c
brandester32 <brandondester@gmail.com>
2016-11-18 00:34:28 -0500

    Update createAccount.php

--- createAccount.php
+++ createAccount.php
@@ -6,15 +6,24 @@
 		</title>
 	</HEAD>
 <BODY>
-	<img src="goodvibeslogo.jpg" width="25%" height="25%" alt="logo"/>
+<STYLE>
+body {
+	background-repeat: no-repeat;
+	background-position: right top;
+	background-size: 25%;
+	background-color: lightblue;
+}
+</STYLE>
+	<a href="loginpage.php"><img src="goodvibeslogo.jpg" width="25%" height="25%" alt="logo" align="left"><a>
+	</br></br></br></br></br></br></br></br></br></br>
 	<hr />
 	<h1 align="center">Create New Account</h1>
-<form align="center" action="createAccount_script.php" method="POST">
+<form align="center" action="createAccount.php" method="POST">
 
 <p>First Name: <input type="text" name = "fname" size = "30"/></p>
 <p>Last Name: <input type="text" name = "lname" size = "30"/></p>
 <p>Enter Desired Username: <input type="text" name="username" size="30"/></p>
-<p>Enter Email: <input type="text" name="email" size="30"/></p>
+<p>Enter Email: <input type="text" name="email" size="30" /></p>
 <p>Enter Desired Password: <input type="password" name="password" size="30"/></p>
 <p>Re-Enter Desired Password: <input type="password" name="repassword" size="30"/></p>
 
