8b44e29f96887ac96ba90c65eb9722b6884e6341
MaxAukshunas <jaukshunas@umass.edu>
2016-11-17 22:44:02 -0500

    Update deleteaccount.php

--- deleteaccount.php
+++ deleteaccount.php
@@ -4,6 +4,10 @@
 	$database = mysqli_connect("localhost","root","password","user_accounts"); // connect to database
 
 	$deleteaccount = mysqli_query($database,"DELETE FROM accounts WHERE username = '$logged_in_user'");
+	$deleteprofile = mysqli_query($database,"DELETE FROM profile WHERE username = '$logged_in_user'");
+
+	header("location: loginpage.php");
+	print "Congrats you deleted your account!";
 
 
 ?>
