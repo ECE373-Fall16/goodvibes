8942e3717ecbdd523858f7fd549ed1bfe2b8e5bc
brandester32 <brandondester@gmail.com>
2016-12-01 20:16:39 -0500

    Update editprofile.php

--- editprofile.php
+++ editprofile.php
@@ -4,7 +4,7 @@
 	$database = mysqli_connect("localhost","root","password","user_accounts"); // connect to database
 
 	$account = mysqli_query($database,"SELECT * FROM profiles WHERE username = '$logged_in_user'"); // find the account of the user
-
+	
 	$accountrow = mysqli_fetch_array($account,MYSQLI_ASSOC); // find the row of the account
 
 	
@@ -13,6 +13,7 @@
 	$instruments = $accountrow['instruments']; 
 	$age = $accountrow['age']; 
 	$experience = $accountrow['experience']; 
+	$scuser = $accountrow['scuser'];
 
 
 ?>
@@ -35,7 +36,7 @@
 <p>Instruments: <input type="text" name="instruments" size="30" value= "<?php print "$instruments" ?>"/></p>
 <p>Age: <input type="text" name="age" size="30" value= "<?php print "$age" ?>"/></p>
 <p>Experience (years): <input type="text" name="experience" size="30" value= "<?php print "$experience" ?>"/></p>
-
+<p>Enter sound cloud link: <input type="text" name="scuser" size="30" value= "<?php print "$scuser" ?>"/></p>
 <input type="submit" name="submit" value="Submit"/>
 </form>
 </BODY>
