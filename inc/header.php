64ccc4017e6d5d626f0e580e85629377707cbafc
brandester32 <brandondester@gmail.com>
2016-12-01 20:21:55 -0500

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!doctype HTML>
+<HTML>
+	<HEAD>
+		<link rel="stylesheet" type="text/css" href="../css/style.css" />
+
+	</HEAD>
+<BODY>
+<div class="headerMenu">
+	<div id="wrapper">
+		<div id="logo">
+			<img src="goodvibeslogo.jpg" height="20%" width="20%"/>
+		</div>
+		<div id="menu" align="center">
+			<a href="profile.php">View Profile</a>
+			<a href="editprofile.php">Edit Profile</a>
+			<a href="friends-directory.php">Member Directory</a>
+			<a href="friends-list.php">Friends List</a>
+			<a href="feed-view.php">View Feed</a>
+			<a href="feed-post.php">Post Status</a>
+			<a href="messages-inbox.php">Inbox</a>
+			<a href="messages-compose.php">Compose</a>
+		</div>
+	</div>
+</div>
+</BODY>
+</HTML>
