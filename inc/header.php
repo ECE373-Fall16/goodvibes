b9f8c865942519bc6fc071096c4532e4b0e3f5a3
brandester32 <brandondester@gmail.com>
2016-12-05 20:11:11 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,11 @@
 
 	</HEAD>
 <BODY>
+<STYLE>
+body {
+	background-color: lightblue;
+}
+</STYLE>
 <div class="headerMenu">
 	<div id="wrapper">
 		<div id="logo">
@@ -13,14 +18,16 @@
 		<div id="menu" align="center">
 			<a href="profile.php">View Profile</a>
 			<a href="editprofile.php">Edit Profile</a>
-			<a href="friends-directory.php">Member Directory</a>
+			<a href="members.php">Member Directory</a>
 			<a href="friends-list.php">Friends List</a>
 			<a href="feed-view.php">View Feed</a>
 			<a href="feed-post.php">Post Status</a>
 			<a href="messages-inbox.php">Inbox</a>
 			<a href="messages-compose.php">Compose</a>
+			<a href="logout.php">Log Out</a>
 		</div>
 	</div>
+	<hr />
 </div>
 </BODY>
 </HTML>
