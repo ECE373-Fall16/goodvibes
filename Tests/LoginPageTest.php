6c0e2a0d267954f068b9118abf7235e174c6c154
liammshea <liammshea@gmail.com>
2016-12-14 17:37:48 -0500

    Update LoginPageTest.php

--- Tests/LoginPageTest.php
+++ Tests/LoginPageTest.php
@@ -3,6 +3,7 @@ namespace ECE373-Fall16/goodvibes/;
 
 use 'loginpage.php';
 
+#This tests if username and password fields are left blank
 class LoginPageTest extends PHPUnit_Framework_TestCase{
   public function blankEntries(){
 	  <form action="loginpage.php" method="POST">
