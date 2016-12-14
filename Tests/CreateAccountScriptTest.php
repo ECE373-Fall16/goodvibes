1b59318711ae377f043ab33a0bf45d20d5e2d180
liammshea <liammshea@gmail.com>
2016-12-14 02:23:44 -0500

    Update CreateAccountScriptTest.php

--- Tests/CreateAccountScriptTest.php
+++ Tests/CreateAccountScriptTest.php
@@ -1,12 +1,18 @@
 <?php
-
 namespace ECE373-Fall16/goodvibes/edit/master/Tests;
 
 use 'createAccount_script.php';
 
 class CreateAccountScriptTest extends PHPUnit_Framework_TestCase{
-  
+  public function blankEntries(){
+	  <form action="createAccount_script.php" method="POST">
+	  $fname = "";
+	  $lname = "";
+	  $username "";
+	  $password = "";
+	  $repassword = "";
+	  $this->assertEquals($error, false);
+  }
   
 }
-
 ?>
