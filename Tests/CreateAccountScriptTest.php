0896882fd5aa8ba08df5a5fe63a32969c2d7234b
liammshea <liammshea@gmail.com>
2016-12-14 00:49:13 -0500

    Update and rename CreateAccountTest.php to CreateAccountScriptTest.php

---
+++ Tests/CreateAccountScriptTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ECE373-Fall16/goodvibes/edit/master/Tests;
+
+use 'createAccount_script.php';
+
+class CreateAccountScriptTest extends PHPUnit_Framework_TestCase{
+  
+  
+}
+
+?>
