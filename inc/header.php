cea96a7910bb9f1cd29fa5cc93f6ecc6c3e8e867
brandester32 <brandondester@gmail.com>
2016-12-13 20:27:28 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,9 @@
 <!doctype HTML>
+<?php
+include('session.php');
+$grab_messages = mysqli_query($database,"SELECT * FROM private_messages WHERE user_to = '$logged_in_user' && opened ='no' && deleted='no'");
+$numrows = mysqli_num_rows($grab_messages);
+?>
 <HTML>
 	<HEAD>
 		<link rel="stylesheet" type="text/css" href="../css/style.css" />
@@ -15,15 +20,13 @@ body {
 		<div id="logo">
 			<img src="goodvibeslogo.jpg" height="20%" width="20%"/>
 		</div>
+		<div align="right"> <?php echo "Hello, ", $username;?></div>
 		<div id="menu" align="center">
 			<a href="profile.php">View Profile</a>
-			<a href="editprofile.php">Edit Profile</a>
 			<a href="members.php">Member Directory</a>
-			<a href="friends-list.php">Friends List</a>
-			<a href="feed-view.php">View Feed</a>
-			<a href="feed-post.php">Post Status</a>
-			<a href="messages-inbox.php">Inbox</a>
-			<a href="messages-compose.php">Compose</a>
+			<a href="bands.php">Band Directory</a>
+			<a href="my_messages.php">Inbox (<?php echo $numrows;?>)</a>
+			<a href="bandRequests.php">Band Requests</a>
 			<a href="logout.php">Log Out</a>
 		</div>
 	</div>
