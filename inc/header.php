39c734ecbbe476681056d8d3cc266c64fc26ea77
brandester32 <brandondester@gmail.com>
2016-12-14 01:13:51 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -3,6 +3,10 @@
 include('session.php');
 $grab_messages = mysqli_query($database,"SELECT * FROM private_messages WHERE user_to = '$logged_in_user' && opened ='no' && deleted='no'");
 $numrows = mysqli_num_rows($grab_messages);
+$grab_requests = mysqli_query($database,"SELECT * FROM band_requests WHERE user_to = '$logged_in_user'");
+$numrows2 = mysqli_num_rows($grab_requests);
+$grab_profile_pic = mysqli_query($database,"SELECT image FROM profiles WHERE username = '$logged_in_user'");
+$image = mysqli_fetch_array($grab_profile_pic);
 ?>
 <HTML>
 	<HEAD>
@@ -19,14 +23,20 @@ body {
 	<div id="wrapper">
 		<div id="logo">
 			<img src="goodvibeslogo.jpg" height="20%" width="20%"/>
-		</div>
-		<div align="right"> <?php echo "Hello, ", $username;?></div>
+			<img align="right" <?php if($image['image'] == "")
+						echo "<img width='150' height='150' src= 'pictures/default.png' alt='Default Profile Pic'>";
+					else
+						echo "<img width='150' height='150' src= 'pictures/".$image['image']."' alt='Profile Pic'>";
+					?>
+			<p align="right"> <?php echo "Hello, ", $username;?> </p>
+			
+
 		<div id="menu" align="center">
 			<a href="profile.php">View Profile</a>
 			<a href="members.php">Member Directory</a>
 			<a href="bands.php">Band Directory</a>
 			<a href="my_messages.php">Inbox (<?php echo $numrows;?>)</a>
-			<a href="bandRequests.php">Band Requests</a>
+			<a href="bandRequests.php">Band Requests (<?php echo $numrows2;?>)</a>
 			<a href="logout.php">Log Out</a>
 		</div>
 	</div>
